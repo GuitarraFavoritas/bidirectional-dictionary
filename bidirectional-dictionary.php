130fc0c287518896f2fa80c5c162c5aec6784b18
fer-unix <juanfer30mejia@gmail.com>
2025-02-23 19:17:55 -0600

    Initial commit: Set up plugin structure

---
+++ bidirectional-dictionary.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Bidirectional Dictionary
+Description: A WordPress plugin for bidirectional translation of languages with CSV import functionality.
+Version: 1.0
+Author: Your Name
+*/
+
+// Your plugin code will go here
